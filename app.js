f8486a3d8831825cdb1f306b0114d8cefad94422
developer-sahadat <mdridoysikder862@gmail.com>
2022-10-27 12:27:47 +0600

    schema ready

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 const express = require("express");
 const app = express();
 var cors = require("cors");
+const { default: mongoose } = require("mongoose");
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+const services = mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: [true, "Please Provide Services Name"],
+  },
+  space: {
+    type: Number,
+    required: true,
+  },
+  food: {
+    type: String,
+    required: true,
+    enum: ["Low Food", "High Food", "All Food"],
+  },
+  budget: {
+    type: Number,
+    required: true,
+  },
+});
 
 module.exports = app;
